e3d8dcab4af213b72dd7f0ba11d73f9c351859fd
Darron B <dadonbay@gmail.com>
2020-02-25 15:38:12 -0600

    Auth testing complete. User can register, login, and logout

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, g
+from flask_login import LoginManager
 from resources.user import user
 import models
 
@@ -7,7 +8,21 @@ PORT = 8000
 
 app = Flask(__name__) 
 
+app.secret_key = "Help is quatum"
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(userid):
+	try:
+		return models.User.get(models.User.id == userid)
+	except models.DoesNotExist:
+		return None
+
+
 app.register_blueprint(user, url_prefix='/api/v1/user')
+
 
 if __name__ == '__main__':
 	models.initialize()
--- resources/user.py
+++ resources/user.py
@@ -1,7 +1,7 @@
 import models
 from flask import Blueprint, request, jsonify
 from flask_bcrypt import generate_password_hash, check_password_hash
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, LoginManager
 from playhouse.shortcuts import model_to_dict
 
 
@@ -23,7 +23,7 @@ def register():
 			status=401
 			), 401
 	except models.DoesNotExist:
-		created_user = models.User.create(
+		created_user=models.User.create(
 			username=payload['username'],
 			email=payload['email'],
 			password=generate_password_hash(payload['password'])
@@ -39,10 +39,58 @@ def register():
 			message=f"successfully registered as {user_dict['email']}", 
 			status=201
 			), 201
-			
+#___________________________________________________________________________________________
 
-# USERS:
-# 		POST '/register' <<--allows users to instatiate themeselves in the app
-# 		POST '/login' <<--takes credentials to use app	
-# 		GET '/logged_in' <<--tracks current user
-# 		GET '/logout' <<--allows session to end	
+@user.route('/login', methods=['POST'])
+def login():
+	payload=request.get_json()
+	# remove the line about --> payload['email'] = payload['email'].lower()
+	payload['username'] = payload['username'].lower()
+
+	try:
+		user = models.User.get(models.User.username == payload['username'])
+		user_dict = model_to_dict(user)
+		password_is_good = check_password_hash(user_dict['password'], payload['password'])
+
+		if password_is_good:
+			login_user(user)
+			user_dict.pop('password')
+			return jsonify(
+				data=user_dict,
+				message=f"Successfully logged in as {user_dict['username']}", 
+				status=200
+				), 200
+		else: 
+			return jsonify(
+				data={},
+				message="Try another password", 
+				status=401
+				), 401
+	except models.DoesNotExist:
+		return jsonify(
+			data={}, 
+			message="email or password is incorrect",
+			# is there a way to tell the user exactly which is no good? 
+			status=401
+			), 401		
+#___________________________________________________________________________________________
+
+# @user.route('logged_in', methods=['GET'])
+# def get_logged_in_user():
+# 	if not current_user.is_authenticated:
+# 		return jsonify(
+# 			data={},
+# 			message="No user is currently logged in",
+# 			status=401
+# 			), 401
+
+#___________________________________________________________________________________________
+
+@user.route('/logout', methods=['GET'])
+def logout():
+	logout_user()
+	return jsonify(
+		data={},
+		message="Successfully logged out",
+		status=200
+		), 200
